348c59730e96a8f003fdb8984a2dcfe06d9d9110
Fahmi Idris <fahmiidrisss08@gmail.com>
2021-06-24 17:21:24 +0700

    Add Route in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,29 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::group(['middleware'=>'auth:sanctum'], function()
+{
+    //Kelola Data Admin
+    Route::post('/admin/create', [AdminController::class, 'createAdmin']);
+    Route::get('/admin/show', [AdminController::class, 'showAdmin']);
+    Route::put('/admin/update/{id}', [AdminController::class, 'updateAdmin']);
+    Route::delete('/admin/delete/{id}', [AdminController::class, 'deleteAdmin']);
+    
+    //Kelola Data Santri
+    Route::post('/santri/create', [SantriController::class, 'createSantri']);
+    Route::get('/santri/show', [SantriController::class, 'showSantri']);
+    Route::get('/santri/detail/{id}', [SantriController::class, 'detailSantri']);
+    Route::put('/santri/update/{id}', [SantriController::class, 'updateSantri']);
+    Route::delete('/santri/delete/{id}', [SantriController::class, 'deleteSantri']);
+    
+    
+    //Logout User
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+//Login User
+Route::post('/login', [AuthController::class, 'login']);
+
+//Tes
+Route::get('/tes', [AuthController::class, 'tes']);
