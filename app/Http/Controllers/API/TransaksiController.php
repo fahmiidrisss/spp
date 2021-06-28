a1fc55b4cf747c557e1f0d40671e0558d4a562db
Fahmi Idris <fahmiidrisss08@gmail.com>
2021-06-28 17:46:17 +0700

    coba baca jumlah bulan

--- app/Http/Controllers/API/TransaksiController.php
+++ app/Http/Controllers/API/TransaksiController.php
@@ -10,30 +10,30 @@ class TransaksiController extends Controller
 {
     public function createTransaksi(Request $request, $jumlah_bulan)
     {
-        $request->validate([
-            'nis'               => 'required',
-            'total_bayar'       => 'required',
-            'spp'               => 'required',
-            'infaq'             => 'required',
-            'id_admin'          => 'required'
-        ]);
+        // $request->validate([
+        //     'nis'               => 'required',
+        //     'total_bayar'       => 'required',
+        //     'spp'               => 'required',
+        //     'infaq'             => 'required',
+        //     'id_admin'          => 'required'
+        // ]);
 
-        // dd($request->all());
-        $transaksi = new Transaksi();
-        $transaksi->nis = $request->nis;
-        $transaksi->total_bayar = 50000;
-        $transaksi->spp = 35000;
-        $transaksi->infaq = 15000;
-        $transaksi->status_transaksi = "Tunai";
-        $transaksi->id_admin = $request->id_admin;
-        for($i = 0; $i < $jumlah_bulan; $i++)
-        {
-            $transaksi->save();
-        }
+        // // dd($request->all());
+        // for($i = 0; $i < $jumlah_bulan; $i++)
+        // {
+        // $transaksi = new Transaksi();
+        // $transaksi->nis = $request->nis;
+        // $transaksi->total_bayar = 50000;
+        // $transaksi->spp = 35000;
+        // $transaksi->infaq = 15000;
+        // $transaksi->status_transaksi = "Tunai";
+        // $transaksi->id_admin = $request->id_admin;
+        //     $transaksi->save();
+        // }
 
         return response()->json([
             'message' => 'Data Transaksi Berhasil Ditambahkan',
-            'data_transaksi' => $transaksi
+            'data_transaksi' => $jumlah_bulan
         ], 200);
     }
 
